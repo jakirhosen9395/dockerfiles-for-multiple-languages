df465edb01fc9c1bf77340f121eb1d696d0821d7
jakirhosen9395 <jakirhosen9395@gmail.com>
2025-07-29 15:38:58 +0600

    added aditional default created  files

--- dotnet-calculator/obj/Debug/net9.0/dotnet-calculator.AssemblyInfo.cs
+++ dotnet-calculator/obj/Debug/net9.0/dotnet-calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9742ef983fc5459636165dfe7ceb561ec2120bed")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
